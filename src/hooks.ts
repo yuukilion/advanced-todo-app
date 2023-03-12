b9f9fd6dfd96536462c9ece8b162f5b559b61249
yuukilion <hy130310@gmail.com>
2023-03-12 12:04:12 +0900

    [add]Reduxの設定を追加

---
+++ src/hooks.ts
@@ -0,0 +1,5 @@
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
+import type { RootState, AppDispatch } from './pages/_app'
+
+export const useAppDispatch: () => AppDispatch = useDispatch
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
---
+++ src/stores/todo.ts
@@ -0,0 +1,35 @@
+import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit/dist/createAction';
+
+export interface TodoState {
+  id: number;
+  title: string;
+  category: string;
+  priority: string;
+  limit: any;
+  detail: string;
+}
+
+const initialState: Array<TodoState> = [];
+
+export const TodoSlice = createSlice({
+  name: 'todo',
+  initialState,
+  reducers: {
+    add: (state, action: PayloadAction<TodoState>) => {
+      return [...state, action.payload];
+    },
+    update: (state, action: PayloadAction<TodoState>) => {
+      const updatedList = [...state].map((todo) =>
+        todo.id === action.payload.id ? action.payload : todo,
+      );
+      return updatedList;
+    },
+    remove: (state, action: PayloadAction<TodoState>) => {
+      const remainingList = [...state].filter(
+        (item) => item.id !== action.payload.id,
+      );
+      return remainingList;
+    },
+  },
+});
